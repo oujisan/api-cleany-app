23e2afb34728a59adc22f621fda4600ee4574e2e
oujisan <oujisan.main@gmail.com>
2025-05-05 09:20:18 +0700

    Mengisi file SqlDbHelper.cs
    Membuat helper untuk menghubungkan dengan PostgreSQL

--- src/Helpers/SqlDbHelper.cs
+++ src/Helpers/SqlDbHelper.cs
@@ -1,6 +1,45 @@
-﻿namespace api_cleany_app.src.Helpers
+﻿using Npgsql;
+
+namespace api_cleany_app.src.Helpers
 {
-    public class SqlDbHelper
+    public class SqlDbHelper : IDisposable
     {
+        private readonly NpgsqlConnection _connection;
+        public string _connectionString;
+
+        public SqlDbHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+            _connection = new NpgsqlConnection(_connectionString);
+        }
+
+        public NpgsqlCommand NpgsqlCommand(string pQuery)
+        {
+            return new NpgsqlCommand(pQuery, _connection);
+        }
+
+        public void OpenConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Dispose();
+            }
+        }
     }
 }
