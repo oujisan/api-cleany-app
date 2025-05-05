db137f05d2166bd7827e68a940bf16704224f3d8
oujisan <oujisan.main@gmail.com>
2025-05-05 09:15:31 +0700

    Modified: User.cs
    Merubah nama file dari 'Register.cs' menjadi 'User.cs' dan mengisi property

---
+++ src/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace api_cleany_app.src.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string imageUrl { get; set; }
+        public int roleId { get; set; }
+    }
+}
