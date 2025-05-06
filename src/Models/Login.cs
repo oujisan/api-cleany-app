e285d155dc56d6c6a4405db9fdc9f4aa8de4aafb
oujisan <oujisan.main@gmail.com>
2025-05-06 15:31:31 +0700

    remove property username form model login

--- src/Models/Login.cs
+++ src/Models/Login.cs
@@ -2,7 +2,6 @@
 {
     public class Login
     {
-        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set;  } 
         public string RoleId { get; set; }
